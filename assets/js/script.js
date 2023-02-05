6bbaa79c2f3399e177f13a9b80342d786c73db36
bea zacharska <beata.zacharska@gmail.com>
2023-02-05 19:15:04 +0000

    setting up the API key and variables to pick relevant HTML elements

---
+++ assets/js/script.js
@@ -0,0 +1,9 @@
+//Setting up the API key
+const APIURL = "https://api.openweathermap.org";
+const APIKey = "aef5ffbba81df0d918fb7c776222cdb5";
+
+//Setting up variables to pick relevant html elements
+let searchInput = $("#search-input");
+let searchForm = $("#search-form");
+
+
