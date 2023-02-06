765751df416e49f75d231f8ef42060f6d701b79c
bea zacharska <beata.zacharska@gmail.com>
2023-02-06 20:53:39 +0000

    setting up variables to store history searches, adding a function to render search history, append it and store in local storage, also updated CSS file, adding a reset CSS

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,4 +6,41 @@ const APIKey = "aef5ffbba81df0d918fb7c776222cdb5";
 let searchInput = $("#search-input");
 let searchForm = $("#search-form");
 
+//Setting up a variable to store history searches
+let searchHistory = [];
+let searchHistoryStorage = $("#history");
+
+//Setting up a function to render search history
+function getSearchHistory() {
+    searchHistoryStorage.html("");
+    for(let i = 0; i < searchHistory.length; i++){
+        let btn =$("<button>");
+        btn.attr("type", "button");
+        btn.addClass("history-btn btn-history");
+        btn.attr("data-search", searchHistory[i]);
+        btn.text(searchHistory[i]);
+        searchHistoryStorage.append(btn);
+    }
+}
+
+function appendSearchHistory(search) {
+    if (searchHistory.indexOf(search) !== -1) {
+        return;
+    }
+    searchHistory.push(search);
+    localStorage.setItem("search-history", JSON.stringify(searchHistory));
+    getSearchHistory()
+}
+
+
+function startHistory(){
+    let storedHistory = localStorage.getItem("search-history");
+    if(storedHistory) {
+        searchHistory = JSON.parse(storedHistory);
+    }
+    getSearchHistory()
+}
+
+startHistory()
+
 
